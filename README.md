bba615a08c94bba62f5e473020226b1d1edb2832
Hiroshi Omata <op.homata@gmail.com>
2020-03-08 20:06:26 +0900

    Initial page

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# nagano-map
+長野の交通・ホテルマップ
+----
+
+Python style one line server
+```
+$ python -m http.server 8000
+```
+
+### MapBox Sample
+* [MapBox Document](https://docs.mapbox.com/jp/)
+* [Mapbox GL JS #008 – 外部ファイルのGeoJSONを表示](https://day-journal.com/memo/mapboxgljs-008/)
+    * [mapboxgljs-starter](https://github.com/dayjournal/mapboxgljs-starter)
+
+### ホテル情報
+* 長野県旅館ホテル組合会 - http://www.nagano-ryokanhotel.com/
+* 長野市ホテル旅館組合 - http://www.nagano-yado.jp/
+
+### アクセス
+* 長野市アクセス - http://www.nagano-yado.jp/access/
+* しなの鉄道 - http://www.shinanorailway.co.jp/
+* 長野電鉄（電車・バス）- http://www.nagaden-net.co.jp/
+* アルピコ川中島バス - http://www.alpico.co.jp/kbc/index.html
+* ＪＲ東日本長野支社 - http://www.jreast.co.jp/nagano/
+* えきねっと - http://jreast.eki-net.com/
+* バス比較ナビ - https://www.bushikaku.net/
+
+### 緯度経度変換
+* CSVアドレスマッチングサービス - http://newspat.csis.u-tokyo.ac.jp/geocode/
---
+++ app.js
@@ -0,0 +1,81 @@
+// Access Token
+mapboxgl.accessToken = 'pk.eyJ1IjoiaG9tYXRhIiwiYSI6ImNrN2lqdjFmcDAyeGwzZXJ3dm1pbzI0MDYifQ.KUJNUxQwLW6VMtj33N3JEA';
+
+// Map Style
+var map = new mapboxgl.Map({
+  style: 'mapbox://styles/mapbox/streets-v11',
+  container: "map",
+  center: [138.188705, 36.643083],
+  zoom: 11
+});
+
+// Map Data Load
+map.on('load', function () {
+    // GeoJSON Load
+    map.addSource('nagano_hotel', {
+        type: 'geojson',
+        data: './nagano.geojson'
+    });
+
+    // setting style
+    var point_list = [
+        [ "nagano_hotel", "#FF0000", 'ホテル' ],
+        [ "japanese_inn", "#00FF00", '旅館'   ],
+        [ "venue",        "#0000FF", '施設'   ]
+    ]
+
+    // setting
+    for (var ii=0; ii<point_list.length; ii++) {
+        var point_id    = point_list[ii][0];
+        var point_color = point_list[ii][1];
+        var point_category = point_list[ii][2];
+
+        map.addLayer({
+            "id": point_id,
+            "type": "circle",
+            "source": "nagano_hotel",
+            "layout": {},
+            "paint": {
+                'circle-color': point_color,
+                'circle-radius': 6,
+                'circle-opacity': 0.8,
+                'circle-stroke-color': '#444444',
+                'circle-stroke-width': 1
+            },
+            'filter': ['==', 'category', point_category]
+        });
+
+        map.on('click', point_id, function (e) {
+            var coordinates = e.lngLat;
+
+            // 属性設定
+            var description =
+                '名前: ' + e.features[0].properties.name + '<br>' +
+                /*'Name: ' + e.features[0].properties.name_e + '<br>' + */
+                '住所: ' + e.features[0].properties.address + '<br>' +
+                '電話番号(TEL): ' + e.features[0].properties.tel;
+            var url = e.features[0].properties.url;
+            if (url != null && url != "null") {
+                description += '<br>URL: ' + "<a href=\"" + url + "\">" + url + "</a>";
+            } 
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+            new mapboxgl.Popup()
+                .setLngLat(coordinates)
+                .setHTML(description)
+                .addTo(map);
+        });
+        // カーソルON,OFF
+        map.on('mouseenter', point_id, function () {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+        map.on('mouseleave', point_id, function () {
+            map.getCanvas().style.cursor = '';
+        });
+    }
+
+});
+
+// show control
+map.addControl(new mapboxgl.NavigationControl());
